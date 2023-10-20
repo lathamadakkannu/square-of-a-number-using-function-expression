2ac73bf0255f862125beabb3cf48f99dcf59b54f
lathamadakkannu <lathamadakkannu@gmail.com>
2023-10-20 21:24:09 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+
+/* square of a number using  function exprssion */
+
+let a=prompt("enter the value")
+
+let square=(num)=>{
+    return num*num
+}
+
+console.log(`The square value of  ${a} is:${square(a)}`);
+
+
+
+ 
